62e7759ae545c4dd39a1d3abc0c2a85d251af630
Frank Purcell <github@frankpurcell.com>
2017-10-16 13:49:21 -0700

    file date changes and fixes for missing files

--- who_date.sh
+++ who_date.sh
@@ -1,7 +1,16 @@
 # used by scripts to tell whether we have new Who's on First data (if new, probably don't want to load)
 WHO_FILE=${WHO_FILE:="$DATA_DIR/whosonfirst/meta/wof-neighbourhood-latest.csv"}
-WHO_AGE=${WHO_AGE:=86400}
 WHO_FILETIME=`stat -c %Y $WHO_FILE`
-WHO_NOW=`date +%s` 
-let WHO_DIFF=$WHO_NOW-$WHO_FILETIME;
 
+UPDATE_WHO=false
+if [ -z "$WHO_FILETIME" ];
+then
+  UPDATE_WHO=true
+else
+  WHO_AGE=${WHO_AGE:=86400}
+  WHO_NOW=`date +%s`
+  let WHO_DIFF=$WHO_NOW-$WHO_FILETIME;
+  if [ $WHO_DIFF -lt $WHO_AGE ]; then
+    UPDATE_WHO=true
+  fi
+fi
